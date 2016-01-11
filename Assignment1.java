db5599542e9398eac33e1595b874a666b7162bef
manojmaurya <manoj.maurya.it@gmail.com>
2016-01-11 10:47:35 +0530

    Create Assignment1.java
    This assignment will test your skills on Variables and Data Types.

---
+++ Assignment1.java
@@ -0,0 +1,18 @@
+package project1;
+
+public class Assignment1 {
+	
+	public static void main(String[] args) {
+		// Declare var1 & var2 as integer type variables
+		int var1 = 5, intResult;
+		
+		// Declare var2 as float type variable
+		float var2 = 200.52f;
+		
+		// var2 is typecast as int & added to var1
+		intResult = var1 + (int) var2;
+		System.out.println(intResult);
+		
+	}
+
+}
