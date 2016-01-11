227abfef85390fd5c985f9bd0b6d14d60adc0525
manojmaurya <manoj.maurya.it@gmail.com>
2016-01-11 10:52:50 +0530

    Assignment4.java
    This assignment will test your skills on variables and Data Types.

---
+++ Assignment4.java
@@ -0,0 +1,22 @@
+package project1;
+
+public class Assignment4 {
+	
+	public static void main(String[] args){
+		
+		int x=5, y=10, a, b, c;
+		
+		a = x + y*2;
+		System.out.println("Value of X+y*2 is "+a);
+		
+		b = x - y + 2;
+		System.out.println("Value of X-y+2 is "+b);
+		
+		c = (x + y)*2;
+		System.out.println("Value of (X+y)*2 is "+c);
+		
+		System.out.println("Value of y%x is "+y%x);
+		
+	}
+
+}
